38d24499882288faab9eb936db5f5d66725950de
junily <junily@yonsei.ac.kr>
2024-08-01 22:55:05 +0900

    update linkedlist

---
+++ linkedlist.py
@@ -0,0 +1,45 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Node:
+    """ Node class
+        
+            up
+             |
+    left - data - right
+             |
+           down
+    """
+
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+        self.up = None
+        self.down = None
+
+
+head = ListNode(0)
+
+curr_node = head
+
+new_node = ListNode(1)
+curr_node.next = new_node
+curr_node=curr_node.next
+
+curr_node.next = ListNode(2)
+curr_node=curr_node.next
+
+curr_node.next = ListNode(3)
+curr_node=curr_node.next
+
+curr_node.next = ListNode(4)
+curr_node=curr_node.next
+
+node=head
+while node:
+    print(node.val)
+    node=node.next
+
